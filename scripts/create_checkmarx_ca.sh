3d0dcfc3e621b5ea4f398f041b5cd6633e734a45
Sami Alajrami <sami@kosli.com>
2025-05-22 21:55:45 +0200

    make it fail

--- scripts/create_checkmarx_ca.sh
+++ scripts/create_checkmarx_ca.sh
@@ -4,5 +4,4 @@ kosli create attestation-type checkmarx-scan \
     --description "Checkmarx Scan." \
     --jq '.CriticalIssues == 0' \
     --jq '.HighIssues == 0' \
-    --jq '.MediumIssues == 0' \
     --org kosli-public
