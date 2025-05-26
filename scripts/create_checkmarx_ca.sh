c6d72b2f1093b2ed7bb6d505c7eee25cc5236289
Sami Alajrami <sami@kosli.com>
2025-05-26 13:50:01 +0200

    switch to use custom attestation for snyk

--- scripts/create_checkmarx_ca.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-kosli create attestation-type checkmarx-scan \
-    --description "Checkmarx Scan." \
-    --jq '.CriticalIssues == 0' \
-    --jq '.HighIssues == 0' \
-    --org kosli-public
